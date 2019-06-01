2dd313b6e3032079be743eb8d4691f450f54d420
SplittyDev <splittydev@protonmail.com>
2019-06-01 19:55:02 +0200

    Extract groups from lines
    - Make PrexEngine iterable
    - Fix EOS detection

--- prex.py
+++ prex.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
 
+import re
 import sys
+from typing import Sequence
 
 class PrexEngine:
-	def __init__(self, source=None):
+	def __init__(self, expr: str, source: str = None):
+		self.eos = False
+		self.expr = re.compile(expr)
 		self.source = sys.stdin if source == None else open(source, "r")
-		print(self.source)
+
+	def readline(self) -> str:
+		# Bail on EOS
+		if self.eos:
+			return None
+		# Read line
+		line = self.source.readline()
+		# Test for EOS
+		if line == '':
+			self.eos = True
+		return None if self.eos else line
+
+	def match(self, line: str) -> Sequence[str]:
+		matches = self.expr.match(line)
+		print(matches)
+		pass
+
+	def __iter__(self):
+		return self
+
+	def __next__(self) -> str:
+		line = self.readline()
+		if line == None:
+			raise StopIteration
+		return line
 
 if __name__ == '__main__':
 	# Parse command line options
@@ -16,4 +44,6 @@ if __name__ == '__main__':
 	parser.add_argument("INPUT", action="store", help="the regex pattern")
 	args = parser.parse_args()
 	# Construct PrexEngine
-	prex = PrexEngine(args.file)
+	prex = PrexEngine(args.INPUT, args.file)
+	for line in prex:
+		groups = prex.match(line)
